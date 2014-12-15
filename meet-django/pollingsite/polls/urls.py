165ff2fbe9fb0bf05f1d74e62dd0de40b5d31985
Yoni Gootkin <yoni13@meet.mit.edu>
2014-12-15 20:06:59 +0200

    one step away

--- meet-django/pollingsite/polls/urls.py
+++ meet-django/pollingsite/polls/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
-	url(r'pizza/$', views.pizza, name="pizza")
-	
+	url(r'pizza/$', views.pizza, name="pizza"),
+
+	url(r'burger/$', views.burger, name="burger"),
+	url(r'hotdog/$', views.hotdog, name="hotdog"),
 )
--- meet-django/pollingsite/polls/views.py
+++ meet-django/pollingsite/polls/views.py
@@ -6,4 +6,13 @@ from models import Question, Answer
 # Remember each function/view the first argument/input has to be request
 
 def pizza(request):
-	return HttpResponse("I love Pizza")
+	poll_id = 2;
+	return render(request, 'index.html', {'my_poll_num': poll_id})
+
+def burger(request):
+	poll_ud = 2;
+	return render(request, 'Robots.html', {'my_poll_num': poll_ud})
+
+def hotdog(request):
+	poll_wd = 2;
+	return render(request, 'contact_me.html', {'my_poll_num': poll_wd})
